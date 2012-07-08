571172733795dcf093257e769e7fa54a2366cf43
Jurian Sluiman <jurian@soflomo.com>
2012-07-08 15:48:17 +0200

    Update files to reflect new changes from Skeleton

--- module/Application/config/module.config.php
+++ module/Application/config/module.config.php
@@ -8,7 +8,6 @@ return array(
         'exception_template'       => 'error/index',
         'template_map' => array(
             'layout/layout' => __DIR__ . '/../view/layout/layout.phtml',
-            'index/index'   => __DIR__ . '/../view/index/index.phtml',
             'error/404'     => __DIR__ . '/../view/error/404.phtml',
             'error/index'   => __DIR__ . '/../view/error/index.phtml',
         ),
