2d0b96c0ffe9a5ed9c5b394acab624c1655dcc01
Jurian Sluiman <jurian@soflomo.com>
2012-07-13 11:54:10 +0200

    Update index.php to reflect latest SkeletonApplication changes

--- public/index.php
+++ public/index.php
@@ -1,17 +1,12 @@
 <?php
-use Zend\ServiceManager\ServiceManager,
-    Zend\Mvc\Service\ServiceManagerConfiguration;
-
+/**
+ * This makes our life easier when dealing with paths. Everything is relative
+ * to the application root now.
+ */
 chdir(dirname(__DIR__));
-require_once 'vendor/autoload.php';
 
-// Get application stack configuration
-$configuration = include 'config/application.config.php';
+// Setup autoloading
+include 'init_autoloader.php';
 
-// Setup service manager
-$serviceManager = new ServiceManager(new ServiceManagerConfiguration($configuration['service_manager']));
-$serviceManager->setService('ApplicationConfiguration', $configuration);
-$serviceManager->get('ModuleManager')->loadModules();
-
-// Run application
-$serviceManager->get('Application')->bootstrap()->run()->send();
+// Run the application!
+Zend\Mvc\Application::init(include 'config/application.config.php')->run()->send();
