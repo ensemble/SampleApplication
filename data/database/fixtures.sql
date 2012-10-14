b7c981c826cabdae37d83fabe2b306b91afba2de
Jurian Sluiman <jurian@soflomo.com>
2012-10-14 13:52:06 +0200

    Update to new schema with new db tables

--- data/database/fixtures.sql
+++ data/database/fixtures.sql
@@ -1,15 +1,64 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost
+-- Genereertijd: 14 Oct 2012 om 13:49
+-- Serverversie: 5.1.63
+-- PHP-Versie: 5.3.14
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 
-INSERT INTO `page` (`id`, `parent_id`, `lft`, `lvl`, `rgt`, `root`, `order`, `route`, `module`, `moduleId`) VALUES
-(1, NULL, 1, 1, 2, NULL, 1, '/', 'welcome', 1),
-(2, NULL, 1, 1, 4, NULL, 2, 'about', 'simple', 1),
-(3, 2, 2, 2, 3, 2, 2, 'sub', 'simple', 2);
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zf2`
+--
+
+--
+-- Gegevens worden uitgevoerd voor tabel `page`
+--
+
+INSERT INTO `page` (`id`, `parent_id`, `lft`, `lvl`, `rgt`, `root`, `order`, `visible`, `route`, `module`, `moduleId`) VALUES
+(1, NULL, 1, 1, 2, NULL, 1, 0, '/', 'welcome', 1),
+(2, NULL, 1, 1, 4, NULL, 2, 0, 'about', 'simple', 1),
+(3, 2, 2, 2, 3, 2, 2, 0, 'sub', 'simple', 2);
+
+--
+-- Gegevens worden uitgevoerd voor tabel `page_metadata`
+--
 
 INSERT INTO `page_metadata` (`id`, `page_id`, `title`, `longTitle`, `shortTitle`, `description`, `keywords`) VALUES
 (1, 1, 'Home', 'Ensemble sample application', NULL, NULL, NULL),
 (2, 2, 'About', NULL, NULL, NULL, NULL),
 (3, 3, 'Sub page', NULL, NULL, NULL, NULL);
 
+--
+-- Gegevens worden uitgevoerd voor tabel `simple_text`
+--
+
 INSERT INTO `simple_text` (`id`, `content`) VALUES
 (1, '<h1>Editable page</h1>\r\n\r\n<p>This is a page where you can modify html. There is also a <a href="about/sub">sub page</a> which you can view and modify. Go to the <a href="admin">admin section</a> to edit both pages.</p>\r\n\r\n<!-- Obviously we change this into a more appropriate WYSIWYG editor. This is just to show the ideas behind the admin interface -->'),
 (2, '<h1>Sub page</h1>\r\n\r\n<p>This page is a child of the About page.</p>');
+
+--
+-- Gegevens worden uitgevoerd voor tabel `user`
+--
+
+
+--
+-- Gegevens worden uitgevoerd voor tabel `user_role`
+--
+
+INSERT INTO `user_role` (`role_id`, `default`, `parent`) VALUES
+('admin', 0, 'guest'),
+('guest', 1, NULL);
+
+--
+-- Gegevens worden uitgevoerd voor tabel `user_role_linker`
+--
+
--- data/database/schema.sql
+++ data/database/schema.sql
@@ -1,4 +1,29 @@
+-- phpMyAdmin SQL Dump
+-- version 3.3.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Machine: localhost
+-- Genereertijd: 14 Oct 2012 om 13:42
+-- Serverversie: 5.1.63
+-- PHP-Versie: 5.3.14
+
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `zf2`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `page`
+--
 
 CREATE TABLE IF NOT EXISTS `page` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -8,24 +33,37 @@ CREATE TABLE IF NOT EXISTS `page` (
   `rgt` int(11) NOT NULL,
   `root` int(11) DEFAULT NULL,
   `order` int(11) DEFAULT NULL,
+  `visible` tinyint(1) NOT NULL,
   `route` varchar(255) NOT NULL,
   `module` varchar(255) NOT NULL,
-  `moduleId` int(11) NOT NULL,
+  `moduleId` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `IDX_2074E575727ACA70` (`parent_id`)
+  KEY `IDX_140AB620727ACA70` (`parent_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `page_metadata`
+--
 
 CREATE TABLE IF NOT EXISTS `page_metadata` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  `page_id` int(11) NOT NULL,
+  `page_id` int(11) DEFAULT NULL,
   `title` varchar(255) NOT NULL,
   `longTitle` varchar(255) DEFAULT NULL,
   `shortTitle` varchar(255) DEFAULT NULL,
   `description` varchar(255) DEFAULT NULL,
   `keywords` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`id`),
-  KEY `page_id` (`page_id`)
+  UNIQUE KEY `UNIQ_CF8B3334C4663E4` (`page_id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `simple_text`
+--
 
 CREATE TABLE IF NOT EXISTS `simple_text` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
@@ -33,5 +71,61 @@ CREATE TABLE IF NOT EXISTS `simple_text` (
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=3 ;
 
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user`
+--
+
+CREATE TABLE IF NOT EXISTS `user` (
+  `user_id` int(11) NOT NULL AUTO_INCREMENT,
+  `username` varchar(255) DEFAULT NULL,
+  `email` varchar(255) DEFAULT NULL,
+  `display_name` varchar(50) DEFAULT NULL,
+  `password` varchar(128) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  UNIQUE KEY `username` (`username`),
+  UNIQUE KEY `email` (`email`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user_role`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role` (
+  `role_id` varchar(255) NOT NULL,
+  `default` tinyint(1) NOT NULL,
+  `parent` varchar(255) DEFAULT NULL,
+  PRIMARY KEY (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Tabelstructuur voor tabel `user_role_linker`
+--
+
+CREATE TABLE IF NOT EXISTS `user_role_linker` (
+  `user_id` int(11) unsigned NOT NULL,
+  `role_id` varchar(255) NOT NULL,
+  PRIMARY KEY (`user_id`,`role_id`),
+  KEY `role_id` (`role_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Beperkingen voor gedumpte tabellen
+--
+
+--
+-- Beperkingen voor tabel `page`
+--
 ALTER TABLE `page`
-  ADD CONSTRAINT `FK_2074E575727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `page` (`id`) ON DELETE SET NULL;
+  ADD CONSTRAINT `FK_140AB620727ACA70` FOREIGN KEY (`parent_id`) REFERENCES `page` (`id`) ON DELETE SET NULL;
+
+--
+-- Beperkingen voor tabel `page_metadata`
+--
+ALTER TABLE `page_metadata`
+  ADD CONSTRAINT `FK_CF8B3334C4663E4` FOREIGN KEY (`page_id`) REFERENCES `page` (`id`);
